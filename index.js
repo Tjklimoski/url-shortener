80bdd8dec66b6e01c279415aa3888f224667119e
TJ Klimoski <tjklimoski@gmail.com>
2023-04-28 20:20:30 -0400

    Render ejs file on home route

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Hi');
+  res.render('index');
 })
 
 app.listen(port, err => err ? console.error(err) : console.log(`index.js running on port ${port}`))
