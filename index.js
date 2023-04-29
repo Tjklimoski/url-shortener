28ac394f0044ae9932ac8ee49704272d52252f1a
TJ Klimoski <tjklimoski@gmail.com>
2023-04-28 20:45:01 -0400

    Setup moongoose connection to DB

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@learning.pp2n17d.mongodb.net/urlShortener?retryWrites=true&w=majority`
+);
 
 app.set('view engine', 'ejs');
 
