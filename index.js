38f47ea92fb9f9831e59a299496afc2e85917c83
TJ Klimoski <tjklimoski@gmail.com>
2023-04-28 21:08:52 -0400

    Import and config dotenv to access env variables

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import urlShortener from './urlShortener.js';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,9 +13,14 @@ mongoose.connect(
 );
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true}));
 
 app.get('/', (req, res) => {
   res.render('index');
 })
+
+app.post('/shortenUrl', (req, res) => {
+  res.redirect('/')
+});
 
 app.listen(port, err => err ? console.error(err) : console.log(`index.js running on port ${port}`))
